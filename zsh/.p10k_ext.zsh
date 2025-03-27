8c991cf890a9534109dbd213b0bcc2b0a3a395d0
Benjamin Eder <eder.ben01@gmail.com>
2025-03-27 07:30:01 +0100

    p10k: detect bun.lock file

--- zsh/.p10k_ext.zsh
+++ zsh/.p10k_ext.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 function prompt_my_bun_version () {
-  if [ -e bun.lockb ]; then
+  if [[ -e bun.lockb || -e bun.lock ]]; then
     bun_version="$(bun --version)"
     p10k segment -f yellow -i "󰳮" -t "${bun_version}"
   fi
